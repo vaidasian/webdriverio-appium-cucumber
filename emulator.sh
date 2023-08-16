31d7489f12e837759197eb623b56a1b24512477c
vsieber-mac <vaidas.sieber@eqs.com>
2023-08-16 11:19:36 +0200

    adapted github-actions emulator;

--- emulator.sh
+++ emulator.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "ANDROID_HOME=~/Library/Android/sdk" >> $GITHUB_ENV
-echo "emulator=~/Library/Android/sdk/emulator" >> $GITHUB_ENV
-echo "PATH=$PATH:$ANDROID_HOME/tools/:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/emulator" >> $GITHUB_ENV
+#echo "ANDROID_HOME=~/Library/Android/sdk" >> $GITHUB_ENV
+#echo "emulator=~/Library/Android/sdk/emulator" >> $GITHUB_ENV
+#echo "PATH=$PATH:$ANDROID_HOME/tools/:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/emulator" >> $GITHUB_ENV
 #appium -v
 #which adb
 #adb devices
 #which emulator
 #emulator -list-avds
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-28;google_apis;x86_64'
-echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n test_device -k 'system-images;android-28;google_apis;x86_64' --force
+echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n Pixel_3a_API_34_extension_level_7_arm64-v8a -k 'system-images;android-28;google_apis;x86_64' --force
 echo $ANDROID_HOME/emulator/emulator -list-avds
 echo "Starting emulator"
-nohup $ANDROID_HOME/emulator/emulator -avd test_device -no-snapshot > /dev/null 2>&1 &
+nohup $ANDROID_HOME/emulator/emulator -avd Pixel_3a_API_34_extension_level_7_arm64-v8a -no-snapshot > /dev/null 2>&1 &
 $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 $ANDROID_HOME/platform-tools/adb devices
 echo "Emulator started"
