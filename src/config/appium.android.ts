ee27b7fe57d973c7cab1ea7d74c32fc9ac81f644
vaidasian <vaidas.sieber@gmail.com>
2023-08-21 13:01:51 +0200

    using SpecFileRetries with same error output;

--- src/config/appium.android.ts
+++ src/config/appium.android.ts
@@ -22,9 +22,9 @@ export const config: WebdriverIO.Config = {
         ],
         capabilities: [{
             platformName: 'Android',
-            browserName: 'chrome',
-            'appium:deviceName': 'emulator-5554', // emulator: emulator-5554, real: R58M7193KHH, pixel: Pixel_3a_API_34_extension_level_7_arm64-v8a
-            'appium:platformVersion': '10', // emulator: 10, real: 11, pixel: 14
+            browserName: 'Chrome',
+            'appium:deviceName': 'emulator-5554', // emulator: emulator-5554, real: R58M7123XXX, studio: Pixel_3a_API_34_extension_level_7_arm64-v8a
+            'appium:platformVersion': '12', // emulator: 10, real: 11, studio: 14
             'appium:automationName': 'UiAutomator2',
             'appium:orientation': 'PORTRAIT',
             'appium:newCommandTimeout': 240,
